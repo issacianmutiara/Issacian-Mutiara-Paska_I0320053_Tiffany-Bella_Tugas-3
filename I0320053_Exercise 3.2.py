8631e457cd5f3f6a9569ce1746600eef55a7e86e
I Mutiara <issacianmutiara@gmail.com>
2021-03-17 19:39:00 +0700

    I0320053_Exercise 3.2

---
+++ I0320053_Exercise 3.2.py
@@ -0,0 +1,7 @@
+#Update nilai dalam list Python
+
+list = ['sosiologi', 'geografi', 1991, 2021]
+print("Nilai ada pada index 2 : ",list[2])
+
+list[2] = 2002
+print("Nilai baru ada pada index 2 : ",list[2])
