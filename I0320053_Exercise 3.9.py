1b0b0658af5a54ebac610b42b65228d0c5da747e
I Mutiara <issacianmutiara@gmail.com>
2021-03-17 21:10:14 +0700

    I0320053_Exercise 3.9

---
+++ I0320053_Exercise 3.9.py
@@ -0,0 +1,11 @@
+tup1 = (1,2,3)
+tup2 = ('sayang', 'semuanya')
+
+#Aksi seperti dibawah ini tidak bisa dilakukan pada tuple python
+#Karena memang nilai pada tuple python tidak bisa diubah
+#tup1[0] = 100;
+
+#Jadi, membuat tuple baru sebagai berikut
+
+tup3 = tup1 + tup2
+print(tup3)
