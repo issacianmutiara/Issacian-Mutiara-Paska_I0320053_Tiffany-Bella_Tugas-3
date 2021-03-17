7aa7432f2daed598ea9f08c995ee1be768c73452
I Mutiara <issacianmutiara@gmail.com>
2021-03-17 20:15:45 +0700

    I0320053_Exercise 3.6

---
+++ I0320053_Exercise 3.6.py
@@ -0,0 +1,8 @@
+#Update dictionary Python
+
+dict = {'Nama': 'Issacian', 'Umur': 18, 'Kelas': 'First'}
+dict['Umur'] = 19; # Mengubah entri yang sudah ada
+dict['Sekolah'] = "Sebelas Maret" # Menambah entri baru
+
+print("dict['Umur'] = ",dict['Umur'])
+print("dict['Sekolah'] = ",dict['Sekolah'])
