fc345538abe8c0d0c6d67da0e138d5f0c2a34ae2
I Mutiara <issacianmutiara@gmail.com>
2021-03-17 21:17:52 +0700

    I0320053_Exercise 3.10

---
+++ I0320053_Exercise 3.10.py
@@ -0,0 +1,11 @@
+tup = ('merah', 'kuning', 'hijau')
+print(tup)
+
+#hapus tuple dengan statement del
+del tup
+
+#membuat kembali tuple baru dengan elemen yang diinginkan
+tup = ("Ayo Ngopi Jam Tujuh")
+print("Setelah menghapus tuple;",tup)
+
+
